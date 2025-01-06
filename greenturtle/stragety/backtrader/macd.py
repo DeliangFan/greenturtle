b4e90f3c9533534363f3fb4775b680c234c83eb0
wsfdl <vanderliang@gmail.com>
2025-01-06 23:47:06 +0800

    add macd with atr

--- greenturtle/stragety/backtrader/macd.py
+++ greenturtle/stragety/backtrader/macd.py
@@ -19,7 +19,7 @@ import backtrader as bt
 from greenturtle.stragety.backtrader import base
 
 
-class MACDStrategy(base.BaseStrategy):
+class RefinedMACDStrategy(base.BaseStrategy):
 
     """ MACD class strategy for backtrader"""
 
@@ -58,3 +58,86 @@ class MACDStrategy(base.BaseStrategy):
         else:
             if diff > -0.01 * self.macd.signal[0]:
                 self.order_target_percent(target=1.0)
+
+
+# MACDWithATRStrategy is copied from backtrader/samples/macd-settings.py
+class MACDWithATRStrategy(base.BaseStrategy):
+    """
+    This strategy is loosely based on some of the examples from the Van
+    K. Tharp book: *Trade Your Way To Financial Freedom*. The logic:
+
+      - Enter the market if:
+        - The MACD.macd line crosses the MACD.signal line to the upside
+        - The Simple Moving Average has a negative direction in the last x
+          periods (actual value below value x periods ago)
+
+     - Set a stop price x times the ATR value away from the close
+
+     - If in the market:
+
+       - Check if the current close has gone below the stop price. If yes,
+         exit.
+       - If not, update the stop price if the new stop price would be higher
+         than the current
+    """
+
+    params = (
+        # Standard MACD Parameters
+        ('macd1', 12),
+        ('macd2', 26),
+        ('macdsig', 9),
+        ('atrperiod', 14),  # ATR Period (standard)
+        ('atrdist', 3.0),   # ATR distance for stop price
+        ('smaperiod', 30),  # SMA Period (pretty standard)
+        ('dirperiod', 10),  # Lookback period to consider SMA trend direction
+    )
+
+    # pylint: disable=too-many-positional-arguments,too-many-arguments
+    def __init__(self,
+                 period_me1=12,
+                 period_me2=26,
+                 period_signal=9,
+                 atr_period=14,
+                 atr_dist=3.0,
+                 sma_period=30,
+                 dir_period=10):
+        super().__init__()
+        self.atr_dist = atr_dist
+        self.pstop = 0
+
+        # pylint: disable=unexpected-keyword-arg,too-many-function-args
+        self.macd = bt.indicators.MACD(self.data,
+                                       period_me1=period_me1,
+                                       period_me2=period_me2,
+                                       period_signal=period_signal)
+
+        # Cross of macd.macd and macd.signal
+        # pylint: disable=unexpected-keyword-arg,too-many-function-args
+        self.mcross = bt.indicators.CrossOver(self.macd.macd, self.macd.signal)
+
+        # To set the stop price
+        self.atr = bt.indicators.ATR(self.data, period=atr_period)
+
+        # Control market trend
+        self.sma = bt.indicators.SMA(self.data, period=sma_period)
+        self.smadir = self.sma - self.sma(-dir_period)
+
+    def next(self):
+        if self.order:
+            return  # pending order execution
+
+        if not self.position:  # not in the market
+            if self.mcross[0] > 0.0 > self.smadir:
+                self.order_target_percent(target=1.0)
+                pdist = self.atr[0] * self.atr_dist
+                self.pstop = self.data.close[0] - pdist
+
+        else:  # in the market
+            pclose = self.data.close[0]
+
+            if pclose < self.pstop:
+                self.order_target_percent(target=0)  # stop met - get out
+            else:
+                pdist = self.atr[0] * self.atr_dist
+                # Update only if greater than
+                self.pstop = max(self.pstop, pclose - pdist)
