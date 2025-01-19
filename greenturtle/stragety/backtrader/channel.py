0ecbceb7272b2cfb4a7db32f76e7543496923b91
wsfdl <vanderliang@gmail.com>
2025-01-19 22:30:57 +0800

    add channel strategy

--- greenturtle/stragety/backtrader/channel.py
+++ greenturtle/stragety/backtrader/channel.py
@@ -13,37 +13,42 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-""" MIM class strategy for backtrader"""
+""" Channel class strategy for backtrader"""
 
-from backtrader.indicators import MovAv
+from backtrader.indicators import Highest, Lowest
 from greenturtle.stragety.backtrader import base
 
 
-class MIMStrategy(base.BaseStrategy):
+class DonchianChannel(base.BaseStrategy):
 
-    """ MIM class strategy for backtrader"""
+    """
+    DonchianChannel class strategy for backtrader.
 
-    params = (
-        # Standard MIM Parameters
-        ('period', 100),
-    )
+    Actually, the channel is not good comparing to the EMVA line.
+    """
 
-    def __init__(self):
+    def __init__(self, short_period=25, long_period=50):
         super().__init__()
-        self.movav = MovAv.Exponential(self.data, period=self.p.period)
+        self.short_period = short_period
+        self.long_period = long_period
+        self.data_close = self.data.close
+        self.long_highest = Highest(self.data.high, period=self.long_period)
+        self.long_lowest = Lowest(self.data.low, period=self.long_period)
+        self.short_highest = Highest(self.data.high, period=self.short_period)
+        self.short_lowest = Lowest(self.data.low, period=self.short_period)
 
     def next(self):
         if self.order:
             return
 
         if self.position:
-            if self.data[0] < self.movav[0]:
+            if self.data_close[0] <= self.short_lowest[-1]:
                 self.order_target_percent_with_log(
                     data=self.data,
                     target=0)
         # not in the market
         else:
-            if self.data[0] > self.movav[0]:
+            if self.data_close[0] >= self.long_highest[-1]:
                 self.order_target_percent_with_log(
                     data=self.data,
                     target=self.target)
