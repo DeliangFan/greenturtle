7c4f00ad30778b2063c90dec4b040e2d8d865292
wsfdl <vanderliang@gmail.com>
2025-02-21 07:48:49 +0800

    introduce database model and syncer

---
+++ greenturtle/cmd/sync_db.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2025 GreenTurtle
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+""" Manage scripts, for example run storage database migration."""
+
+import argparse
+
+import munch
+import yaml
+
+from greenturtle.db.migrations import migration
+
+
+parser = argparse.ArgumentParser(
+    prog='GreenTurtle for trading',
+    description='scripts for create the database for greenturtle')
+
+parser.add_argument(
+    "--conf",
+    type=str,
+    default="/etc/greenturtle/greenturtle.yaml",
+    help="config file for greenturtle"
+)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    with open(args.conf, encoding="utf-8") as f:
+        # load the config and convert to object like config
+        conf_dict = yaml.safe_load(f)
+        conf = munch.DefaultMunch.fromDict(conf_dict)
+
+        # do migrate
+        migration.create_all(conf)
--- greenturtle/db/models.py
+++ greenturtle/db/models.py
@@ -13,4 +13,36 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-"""models for the database."""
+"""models for the greenturtle database."""
+
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import DeclarativeBase
+
+
+# pylint: disable=too-few-public-methods
+class Base(DeclarativeBase):
+    """base model"""
+
+
+# pylint: disable=too-few-public-methods
+class Contract(Base):
+    """contract model"""
+    __tablename__ = 'contract'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(DateTime, index=True, nullable=False)
+    expire = Column(DateTime)
+    variety = Column(String(255), index=True, nullable=False)
+    name = Column(String(255), index=True, nullable=False)
+    source = Column(String(255), nullable=False)
+    country = Column(String(255), nullable=False)
+    exchange = Column(String(255))
+    group = Column(String(255))
+    open = Column(Float, default=None)
+    high = Column(Float, default=None)
+    low = Column(Float, default=None)
+    close = Column(Float, default=None)
+    volume = Column(Integer, default=0)
+    open_interest = Column(Integer, default=0)
+    total_volume = Column(Integer, default=0)
+    total_open_interest = Column(Integer, default=0)
